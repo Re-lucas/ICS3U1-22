c6fd02d0e9b32809de296262673cac90cfa461f1
Lu'cas <wuw81013@gmail.com>
2024-02-06 10:08:58 -0500

    saved

---
+++ Java Program/Output1/Output1.java
@@ -0,0 +1,9 @@
+//Feb 6,2024
+//Creater:Lucas Wu
+
+public class Output1{
+   public static void main(String[] args){
+      System.out.println("My name is Lucas Wu");
+      System.out.println("May I not say my address?");
+   }
+}
