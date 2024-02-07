fb73af8950c78c6ee96dd7a5996aab2dea002027
Lu'cas <wuw81013@gmail.com>
2024-02-07 11:08:56 -0500

    Update ProgramHeaderTemple.java

--- ProgramHeaderTemple.java
+++ ProgramHeaderTemple.java
@@ -1,8 +1,7 @@
 /** 
-	File Name: ProgramHeaderExample.java
-	Name: John Doe
-	Class: ICS3U1-01
-	Date: February 21, 2013
-	Description: This program prompts user for the students' individual, then
-	calculates and outputs the average
+	File Name: 
+	Name: 
+   Class: ICS3U1-01
+	Date: 
+	Description: 
 */
