aff73b1921d5d498df4d4d8c58e153676e37c872
Lu'cas <wuw81013@gmail.com>
2024-02-08 09:28:30 -0500

    Create Output_Example_001.java

---
+++ Java Program/Output_Example_001.java
@@ -0,0 +1,17 @@
+/** 
+	File Name: Output_Example_001.java
+   Name: Lucas Wu
+	Class: ICS3U1-01
+	Date: Feb 8, 2024
+	Description: This is a program print out author's name and the address.
+*/
+
+public class Output_Example_001{
+   public static void main (String[] args){
+      System.out.println("Hello World");
+      System.out.println("Hello World");
+      
+      System.out.println("hello")
+      System.out.println("World")
+      }
+}
