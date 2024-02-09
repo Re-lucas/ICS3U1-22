41c953e817d07bf0279d7ba2f21272f0aeaa1e96
Lu'cas <wuw81013@gmail.com>
2024-02-09 11:18:14 -0500

    Update HW2_Output1.java

--- Java Program/Honework_002/HW2_Output1.java
+++ Java Program/Honework_002/HW2_Output1.java
@@ -6,51 +6,22 @@
 	Description: HW2_Output1.java
 */
 
- // A Java program to display a tic-tac-toe board with an X in the center
-public class HW2_Output1{
- public static void main (String[] args){
-        // Create a 2D array to store the board
-        char [] [] board = new char [3] [3];
-
-        // Fill the board with spaces
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board [i] [j] = ' ';
-            }
-        }
-
-        // Place an X in the center
-        board [1] [1] = 'X';
-
-        // Display the board
-        printBoard (board);
-    }
-
-    // A method to print the board
-    public static void printBoard (char [] [] board) {
-
-        // Print the top border
-        System.out.println ("| | | |");
-
-        // Print each row of the board
-        for (int i = 0; i < 3; i++) {
-
-            // Print the left border
-            System.out.print ("| ");
-
-            // Print each cell of the row
-            for (int j = 0; j < 3; j++) {
-                System.out.print (board [i] [j] + " | ");
-            }
-
-            // Print a new line
-            System.out.println ();
-
-            // Print the bottom border
-            System.out.println ("| | | |");
-        }
-    }
-}
-
+public class  HW2_Output1{
+   public static void main (String[] args) {
+      System.out.println("   " + "|" + "   " + "|" + "   " );
+      System.out.println("   " + "|" + "   " + "|" + "   " );
+      System.out.println("   " + "|" + "   " + "|" + "   " );
+      
+      System.out.println("-----------");
+      
+      System.out.println("   " + "|" + "   " + "|" + "   " );
+      System.out.println("   " + "|" + " x " + "|" + "   " );
+      System.out.println("   " + "|" + "   " + "|" + "   " );
+      
+      System.out.println("-----------");
+      
+      System.out.println("   " + "|" + "   " + "|" + "   " );
+      System.out.println("   " + "|" + "   " + "|" + "   " );
+      System.out.println("   " + "|" + "   " + "|" + "   " );
    }
 }
