464f6308cb4779b5cde0f26d35c608518f941544
Lu'cas <wuw81013@gmail.com>
2024-02-09 11:24:24 -0500

    Create HW2_Output2.java

---
+++ Java Program/Honework_002/HW2_Output2.java
@@ -0,0 +1,20 @@
+/*
+	File Name: HW2_Output1.java
+   Name: Lucas Wu
+	Class: ICS3U1-01
+	Date: Feb 8, 2024
+	Description: HW2_Output1.java
+*/
+
+public class HW2_Output2{
+public static void main(String[] args){
+System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+System.out.println("\\" + "             " + "\\");
+System.out.println("\\" + "             " + "\\");
+System.out.println("\\" + "             " + "\\");
+System.out.println("\\" + "             " + "\\");
+System.out.println("\\" + "             " + "\\");
+System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+
+}
+}
