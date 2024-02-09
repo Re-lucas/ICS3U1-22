6cc146901c4c2c92140429bcbd56c646a35946bc
Lu'cas <wuw81013@gmail.com>
2024-02-09 12:13:29 -0500

    Create HW3_Output2.java

---
+++ Java Program/Homework_003/HW3_Output2.java
@@ -0,0 +1,14 @@
+/*
+	File Name: HW3_Output2.java
+   Name: Lucas Wu
+	Class: ICS3U1-01
+	Date: Feb 8, 2024
+	Description: 
+   
+*/
+
+public class HW3_Output2{
+public static void main(String[] args){
+
+}
+}
