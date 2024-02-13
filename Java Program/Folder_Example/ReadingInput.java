4b63ff02331268536c628c4e910913888d4938dc
Lu'cas <wuw81013@gmail.com>
2024-02-13 11:05:26 -0500

    new

---
+++ Java Program/Folder_Example/ReadingInput.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ReadingInput {
+    public static void main(String[] args) {
+        // Create a Scanner object to read user input
+        Scanner myObj = new Scanner(System.in);
+
+        // Prompt the user to enter their first name
+        System.out.println("Enter your first name");
+        String Name_first = myObj.nextLine(); // Read the user's first name
+
+        // Prompt the user to enter their last name
+        System.out.println("Enter your last name");
+        String Name_last = myObj.nextLine(); // Read the user's last name
+
+        // Declare a variable to store the user's age
+        int user_age;
+        // Prompt the user to enter their age
+        System.out.println("Enter your age");
+        user_age = myObj.nextInt(); // Read the user's age
+
+        // Display the user's full name
+        System.out.println("Your Name is: " + Name_first + " " + Name_last);
+        // Display the user's age
+        System.out.print("Your age is: " + user_age);
+    }
+}
