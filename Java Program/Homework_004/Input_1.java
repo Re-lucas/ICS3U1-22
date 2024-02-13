b76d8e85845628f318b3659d8dc7ce37386e5d44
Lu'cas <wuw81013@gmail.com>
2024-02-13 14:21:47 -0500

    Update Input_1.java

--- Java Program/Homework_004/Input_1.java
+++ Java Program/Homework_004/Input_1.java
@@ -6,8 +6,37 @@
 	Description: 
 */
 
+
 public class Input_1 {
     public static void main(String[] args){
-        
+
+        //Select (with reasons) those names that should not be used as identifiers of Java variables.
+
+        //This name is suitable for a variable because it is descriptive and meaningful.
+        //firstChoice
+
+        //Although it is a keyword in Java, it is not recommended as a variable name because it lacks clarity and does not convey a specific meaning.
+        //this
+
+        //Hyphens are not allowed in variable names. Therefore, this name should not be used.
+        //second-time
+
+        //Variable names cannot start with a number. Hence, this name is invalid.
+        //3rdTime
+       
+        //This name is appropriate for a variable as it describes a specific concept.
+        //monthly_total
+
+        //While it starts with an uppercase letter, it is still a valid variable name.
+        //Last
+
+        //Although it contains a special character (‘$’), it is acceptable as a variable name.
+        //valueIn$ 
+
+        //This name is meaningless and lacks context. It should be avoided.
+        //x27 
+
+        //Variable names cannot have spaces. Therefore, this name is not valid.
+        //word length
     }
 }
