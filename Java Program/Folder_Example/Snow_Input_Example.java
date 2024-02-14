47baca89b032075777cfa9125960d8e13a70b823
Lu'cas <wuw81013@gmail.com>
2024-02-14 10:06:38 -0500

    Create Snow_Input_Example.java

---
+++ Java Program/Folder_Example/Snow_Input_Example.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Snow_Input_Example {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+
+        int cm_snow;
+        Double hour_Homework;
+
+        System.out.print("How many centimeters of snow today? Please enter the number here: ");
+        cm_snow = myObj.nextInt();
+
+        System.out.println("The snow depth at Taogay is " + cm_snow + " centimeters.");
+
+        System.out.print("How many hours have you spent on today's homework? ");
+        hour_Homework = myObj.nextDouble();
+
+        System.out.println("You have spent " + hour_Homework + " hours on today's homework.");
+
+        myObj.close();
+    }
+}
