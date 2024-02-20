11457a9d554d956fa982a245a862b71ec984efb8
Lu'cas <wuw81013@gmail.com>
2024-02-20 10:03:33 -0500

    Update PizzaCost.java

--- Java Program/Homework_005/PizzaCost.java
+++ Java Program/Homework_005/PizzaCost.java
@@ -13,31 +13,37 @@
 import java.util.Scanner;
 
 public class PizzaCost {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
 
-        //Variable
-        final double labour_cost = 0.75;
-        final double rent_cost = 1.00;
-        double diameter = 0.00;
-        double materials_pizza = 0.05;
+        // Constants
+        final double labourCost = 0.75;
+        final double rentCost = 1.00;
+        final double materialsCostFactor = 0.05;
 
-        //Equation
-        double dia_equation = materials_pizza * diameter * diameter; 
-        double total_cost = dia_equation + labour_cost + rent_cost;
+        // Variables
+        double diameter;
+        double materialsCost;
+        double totalCost;
 
-        //Asking the user for the diameter for the pizza
-        System.out.println("Labour cost is $0.75 per pizza\n" + "Rent cost is $1.00 per pizza\n" + "Materials is $0.05*diameter*diameter");
-        System.out.println("Please enter the size (inch) of teh pizza of what you want, and it will display the cost of the pizza: ");
+        // Asking the user for the diameter for the pizza
+        System.out.println("Labour cost is $0.75 per pizza");
+        System.out.println("Rent cost is $1.00 per pizza");
+        System.out.println("Materials cost is $0.05 * diameter * diameter");
+
+        System.out.print("Please enter the size (inch) of the pizza you want: ");
         diameter = myScanner.nextDouble();
 
-        System.out.println("Please type in your materials of your pizza: ");
-        materials_pizza = myScanner.nextDouble();
+        System.out.print("Please enter the materials cost of your pizza: ");
+        materialsCost = myScanner.nextDouble();
 
-        System.out.println("Here is the total cost of " + diameter + "inch of of pizza: \n" + total_cost );
+        // Equation
+        double materialsCostCalculation = materialsCostFactor * diameter * diameter;
+        totalCost = materialsCost + labourCost + rentCost + materialsCostCalculation;
+
+        // Display the total cost formatted to two decimal places
+        System.out.printf("Here is the total cost of %.2f inch pizza: $%.2f%n", diameter, totalCost);
 
         myScanner.close();
-
     }
-    
 }
