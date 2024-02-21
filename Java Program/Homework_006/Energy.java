4374ba81a2c1ba905a4f006d0651a0006ee19552
Lu'cas <wuw81013@gmail.com>
2024-02-21 10:33:26 -0500

    不过是些许风霜罢了

---
+++ Java Program/Homework_006/Energy.java
@@ -0,0 +1,24 @@
+/* 
+    File Name: Energy.java
+    Name: Lucas Wu
+    Date: Feb 21, 2024
+    Course: ICS3U1
+    Description:Einstein’s famous formula, e=mc2, gives the amount of energy 
+    released by the complete conversion of matter of mass m into energy e. If m
+    represents the mass in kilograms and c represents the speed of light in 
+    meters per second (3.0 * 108 m/s), then the result is in the energy units 
+    Joules. It takes 360000 Joules to light a 100-watt light bulb for an hour. 
+    Create a program that prompts that user for a mass in kilograms and then 
+    displays the energy and the number of light bulbs that could be powered.
+
+*/
+import java.util.Scanner;
+
+public class Energy {
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+
+        myScanner.close();
+    }
+    
+}
