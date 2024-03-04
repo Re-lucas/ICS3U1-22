9d7fffd2538d9a2b79e35d6eab2810ea3a8c1b25
Lu'cas <wuw81013@gmail.com>
2024-03-04 10:10:35 -0500

    Update Login.java

--- Java Program/Folder_Example/Login.java
+++ Java Program/Folder_Example/Login.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
 
-public class Login{
-   public static void main(String[] args){
-      Scanner myScanner = new Scanner(System.in);
-   
-      final int user_password = 348905555;
-      int password_check;
-   
+public class Login {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
 
-      System.out.print("Please enter your password: "); 
-      password_check = myScanner.nextInt();
+        final int user_password = 348905555;
+        int password_check;
+        int attempts = 3; // Number of allowed login attempts
 
-      if(password_check == user_password){
-        System.out.println("Login Successfully");
+        while (attempts > 0) {
+            System.out.print("Please enter your password: ");
+            password_check = myScanner.nextInt();
 
-      } else if (password_check != user_password ) {
-        System.out.println("Login Unsuccessfully");
+            if (password_check == user_password) {
+                System.out.println("Login Successfully");
+                break; // Exit the loop if the password is correct
+            } else {
+                System.out.println("Login Unsuccessfully. " + (--attempts) + " attempts remaining.");
+            }
+        }
 
-      }
+        if (attempts == 0) {
+            System.out.println("Too many unsuccessful attempts. Account locked.");
+        }
 
-      myScanner.close();
-
-   }
+        myScanner.close();
+    }
 }
