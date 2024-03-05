2dcd06bcfab67ff558320d26180e1d127d0db777
Lu'cas <wuw81013@gmail.com>
2024-03-05 10:52:00 -0500

    Create Homework_011_1.java

---
+++ Java Program/Homework_011/Homework_011_1.java
@@ -0,0 +1,21 @@
+/*
+	File Name: Homework_011_1.java
+	Name: Lucas Wu
+   	Class: ICS3U1-01
+	Date: Mar 5, 2024
+	Description: 
+*/
+
+public class Homework_011_1 {
+    /*
+    
+     Evaluate the following boolean expressions to true or false
+    a) 9 < 3
+    b) 5 >= 5
+    c) 8!= 64 % 8 
+    d) ‘a’ = = ‘A’
+    e) ‘a’ > ‘A’
+
+     */
+    
+}
