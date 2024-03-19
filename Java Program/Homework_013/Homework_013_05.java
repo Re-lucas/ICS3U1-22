4202721077bf4a7c9993c56ec0c0e18a03d15d8b
Lu'cas <wuw81013@gmail.com>
2024-03-19 11:28:09 -0400

    Update Homework_013_05.java

---
+++ Java Program/Homework_013/Homework_013_05.java
@@ -0,0 +1,34 @@
+/*
+	File Name: Homework_013_05.java
+	Name: Lucas Wu
+   	Class: ICS3U1-01
+	Date: Mar 18,2024
+	Description: 
+*/
+
+public class Homework_013_05 {
+    /*
+    Consider the following statements
+    if (age < minAge) {
+    if (income > minIncome) {
+    System.out.println(“Accept”);
+    } else {
+    System.out.println(“Reject”);
+    }
+    }
+
+    What will the statement print if
+    a) age > minAge and income < minIncome?
+    If age > minAge and income < minIncome, the outer if condition age < minAge will evaluate to false, so the inner if statement will not be executed. Therefore, nothing will be printed.
+
+    b) age < minAge and income < minIncome?
+    If age < minAge and income < minIncome, both conditions age < minAge and income > minIncome will evaluate to true, so the inner if statement will be executed. It will print "Reject".
+
+    c) age > minAge and income > minIncome?
+    If age > minAge and income > minIncome, the outer if condition age < minAge will evaluate to false, so the inner if statement will not be executed. Therefore, nothing will be printed.
+
+    d) age < minAge and income > minIncome?
+    If age < minAge and income > minIncome, both conditions age < minAge and income > minIncome will evaluate to true, so the inner if statement will be executed. It will print "Accept".
+     */
+    
+}
