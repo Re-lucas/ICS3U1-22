d7ee1a7c200a9a41309c57f6e43146985db6c636
Lu'cas <wuw81013@gmail.com>
2024-03-28 09:39:28 -0400

    updata

---
+++ Java Program/Homework_016/Guess.java
@@ -0,0 +1,14 @@
+/*
+	File Name: Count1.java
+	Name: Lucas WU
+   	Class: ICS3U1-01
+	Date: Mar 28, 2024
+	Description: 
+*/
+
+public class Guess {
+    public static void main(String[] args){
+        
+    }
+    
+}
