9df487dd025c087e3bc52e6c031191281a54cd9b
Lu'cas <wuw81013@gmail.com>
2024-03-28 09:31:21 -0400

    Create Count1.java

---
+++ Java Program/Homework_016/Count1.java
@@ -0,0 +1,11 @@
+public class Count1{
+    public static void main(String[] args){
+        int num = 0;
+        int count_num = 50;
+
+        while (num <= count_num){
+            System.out.println(num);
+            num += 5;
+        }
+    }
+}
