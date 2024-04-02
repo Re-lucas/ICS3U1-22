d261ad0372396d76b8f17c9ea0b116ba7edf8300
Lu'cas <wuw81013@gmail.com>
2024-04-02 09:57:38 -0400

    Update CountInt1.java

--- Java Program/Homework_018/CountInt1.java
+++ Java Program/Homework_018/CountInt1.java
@@ -1,5 +1,22 @@
 import java.util.Scanner;
 
 public class CountInt1 {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int count = 0; 
+        int userInput; 
+
+        do {
+            System.out.print("Please enter a interger（-1 for end）：");
+            userInput = input.nextInt();
+
+            if (userInput != -1) {
+                count++;
+            }
+        } while (userInput != -1);
+
+        System.out.println("the total input are ：" + count);
+        
+        input.close();
+    }
 }
