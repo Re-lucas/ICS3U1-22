400a91b976352b2130f6b5763322badd147af08e
Lu'cas <wuw81013@gmail.com>
2024-04-09 11:18:52 -0400

    Update Backward1.java

---
+++ Java Program/Homework_020/Backward1.java
@@ -0,0 +1,8 @@
+public class Backward1 {
+    public static void main(String[] args) {
+        // Start the count from 100
+        for (int count = 100; count >= 5; count -= 5) {
+            System.out.print(count + " ");
+        }
+    }
+}
