3ccd803a115da6aa68386d724c27b2befe83f024
Lu'cas <wuw81013@gmail.com>
2024-04-15 10:59:43 -0400

    Update StudentNames.java

--- Java Program/Folder_Example/StudentNames.java
+++ Java Program/Folder_Example/StudentNames.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class StudentNames {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] studentNames = new String[10];
+
+        System.out.println("Enter the number of Students: ");
+        int numStudent = scanner.nextInt();
+
+        String[] studentNames = new String[numStudent];
         
         for (int i = 0; i < studentNames.length; i++) {
             System.out.print("Enter the name of student " + (i + 1) + ": ");
