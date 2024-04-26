eaff01209e10ad070529fe09e33f89b25a34a3b7
Lu'cas <wuw81013@gmail.com>
2024-04-26 09:50:46 -0400

    Create DayCounter.java

---
+++ Java Program/Folder_Example/DayCounter.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class DayCounter{
+    public void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        final int CONDITION = 0;
+
+        int days = 0;
+        int count = 0;
+        int num_pratice = 0;
+        int num_day = 0;
+
+        System.out.println("Enter the pratice , -1 to indicate no practive:");
+        num_day = myScanner.nextInt();
+        num_pratice = num_day;
+
+        while(num_day!= CONDITION){
+            System.out.println("Enter pratice time:");
+            count ++;
+
+            if (num_day < num_pratice) {
+                num_pratice = num_day;
+                days = count;
+            }
+        }
+
+        System.out.println("Days" + days + ":" + num_pratice + "are the shortest.");
+        System.out.println("You pratice" + days + "in a row");
+
+        myScanner.close();
+    }
+}
