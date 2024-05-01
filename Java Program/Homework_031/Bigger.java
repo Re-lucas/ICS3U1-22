2068ee05d906811958e42b6f3adf178d6391830e
Lu'cas <wuw81013@gmail.com>
2024-05-01 10:45:49 -0400

    Create Bigger.java

---
+++ Java Program/Homework_031/Bigger.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Bigger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        double number1 = scanner.nextDouble();
+
+        System.out.println("Enter the second number:");
+        double number2 = scanner.nextDouble();
+
+        double biggerNumber = Math.max(number1, number2);
+
+        System.out.println("The bigger number is: " + biggerNumber);
+
+        scanner.close();
+    }
+}
