69da86872184f29346ac32f720c2a2e98bc89936
Lu'cas <wuw81013@gmail.com>
2024-05-01 10:43:52 -0400

    Update TaxiCalculator.java

--- Java Program/Homework_031/TaxiCalculator.java
+++ Java Program/Homework_031/TaxiCalculator.java
@@ -2,17 +2,20 @@ import java.util.Scanner;
 
 public class TaxiCalculator {
     public static void main(String[] args) {
-        Scanner myscScanner = new Scanner(System.in);
+        Scanner myScanner = new Scanner(System.in);
 
         int totalPassengers = 0; // Replace with the actual number of passengers
 
         System.out.println("Enter the number of passengers:");
-        
+        totalPassengers = myScanner.nextInt();
+
 
         // Calculate the number of taxis needed
         int taxisRequired = (totalPassengers + 3) / 4;
 
         System.out.println("Total passengers: " + totalPassengers);
         System.out.println("Taxis required: " + taxisRequired);
+
+        myScanner.close();
     }
 }
