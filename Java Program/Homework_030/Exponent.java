b8dec87e216f4e90f251c5467500cc5ca3759dec
Lu'cas <wuw81013@gmail.com>
2024-05-01 11:54:50 -0400

    Create Exponent.java

---
+++ Java Program/Homework_030/Exponent.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exponent {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the base (b):");
+        double base = scanner.nextDouble();
+
+        System.out.println("Enter the exponent (e):");
+        double exponent = scanner.nextDouble();
+
+        double result = Math.pow(base, exponent);
+
+        System.out.println("The result of " + base + " raised to the power of " + exponent + " is: " + result);
+    }
+}
