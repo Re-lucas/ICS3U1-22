b6edf50f4376cdc2ebf31d0a880de036e34de3f5
Lu'cas <wuw81013@gmail.com>
2024-05-02 09:25:49 -0400

    Create StringEx.java

---
+++ Java Program/Folder_Example/StringEx.java
@@ -0,0 +1,12 @@
+public class StringEx {
+    public static void mian(String[] args){
+        int number_1 = 109;
+        int number_2 = 109;
+
+        System.out.println(number_1 = number_2);
+
+        String string_1 = "Hello";
+
+    }
+    
+}
