4ba43dc76116ec3a0a10bdbd18c87f9c283cf5d3
Lu'cas <wuw81013@gmail.com>
2024-05-02 09:45:00 -0400

    Update StringEx.java

--- Java Program/Folder_Example/StringEx.java
+++ Java Program/Folder_Example/StringEx.java
@@ -17,9 +17,12 @@ class StringEx {
 
         string_3 = string_1;
 
+
+        //Comparing if the two variables have the same address(references)
         System.out.println("string_1 == String_2: " + (string_1 == string_2));
         System.out.println("string_2 == String_3:" + (string_2 == string_3));
 
+        //Comparomg if the two variables have the same content
         System.out.println("string_1.equals(string_2)" + string_1.equals(string_2));
         System.out.println("string_1.equals(string_3)" + string_1.equals(string_3));
         System.out.println("");
