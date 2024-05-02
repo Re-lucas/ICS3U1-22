34a55578fca3524a4772edba197057ff94c1a0f8
Lu'cas <wuw81013@gmail.com>
2024-05-02 09:34:30 -0400

    Update StringEx.java

--- Java Program/Folder_Example/StringEx.java
+++ Java Program/Folder_Example/StringEx.java
@@ -1,11 +1,28 @@
-public class StringEx {
+import java.util.Scanner;
+
+class StringEx {
     public static void mian(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+
         int number_1 = 109;
         int number_2 = 109;
-
-        System.out.println(number_1 = number_2);
+        System.out.println(number_1 == number_2);
 
         String string_1 = "Hello";
+        String string_2;
+        String string_3;
+
+        System.out.println("string_2: ");
+        string_2 = myScanner.nextLine();
+
+        string_3 = string_1;
+
+        System.out.println("string_1 == String_2: " + (string_1 == string_2));
+        System.out.println("string_2 == String_3:" + (string_2 == string_3));
+
+        
+
+
 
     }
     
