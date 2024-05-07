0b5477adede13f7688a420bb5362d988b4277f6d
Lu'cas <wuw81013@gmail.com>
2024-05-07 11:22:25 -0400

    Update AddDigits.java

---
+++ Java Program/Homework_034/AddDigits.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AddDigits{
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        String input = myScanner.nextLine();
+        int sum = 0;
+
+
+        myScanner.close();
+    }
+}
