01a3a1531a7c6e34c6fcc0c2bf11c117d6f81f58
Lu'cas <wuw81013@gmail.com>
2024-05-08 09:32:45 -0400

    Update AddPairs.java

--- Java Program/Homework_034/AddPairs.java
+++ Java Program/Homework_034/AddPairs.java
@@ -5,9 +5,27 @@ public class AddPairs{
         Scanner myScanner = new Scanner(System.in);
 
         System.out.println("Enter a String of number: ");
-        String input = scanner.next();
+        String input = myScanner.next();
         int sum = 0;
 
+        for (int i = 0; i < input.length(); i += 2) {
+            int firstDigit = input.charAt(i) - '0' ;
+
+
+            int secondDigit = 0;
+
+            if(i + 1 < input.length() ){
+                secondDigit = input.charAt(i + 1) - '0';
+            }
+
+            int twoDigitNumber = firstDigit * 10 + secondDigit; 
+            sum += twoDigitNumber;
+
+            System.out.println("The sum of two String of number are: " + sum);
+        }
+
+        myScanner.close();
+
     }
-    
 }
+    
