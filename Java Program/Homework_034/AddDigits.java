99ef5a2063faed9c70d65ee1899f9365b6805363
Lu'cas <wuw81013@gmail.com>
2024-05-08 09:17:09 -0400

    Update AddDigits.java

--- Java Program/Homework_034/AddDigits.java
+++ Java Program/Homework_034/AddDigits.java
@@ -8,6 +8,18 @@ public class AddDigits{
         String input = myScanner.nextLine();
         int sum = 0;
 
+        //by using a chatAt() method to loop over each number
+        for (int i = 0; i < input.length(); i++){
+            char digitChar = input.charAt(i); //get the current number
+            int digit = Character.getNumericValue(digitChar);//the method can convert char into digit
+
+            //there is another method by no using getNumbericValue() 
+            //int digit = digitChar - '0' ;
+             
+            sum += digit;
+        }
+
+        System.out.println("The number of the sum is: " + sum);
 
         myScanner.close();
     }
