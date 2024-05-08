7ba37dfec169980d4479e33e87436f510bafd5e5
Lu'cas <wuw81013@gmail.com>
2024-05-08 09:25:12 -0400

    Create AddPairs.java

---
+++ Java Program/Homework_034/AddPairs.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class AddPairs{
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter a String of number: ");
+        String input = scanner.next();
+        int sum = 0;
+
+    }
+    
+}
