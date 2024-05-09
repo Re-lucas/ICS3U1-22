55187b9caecff59b3f9493472b176d7e6a750a1a
Lu'cas <wuw81013@gmail.com>
2024-05-09 14:49:45 -0400

    Update Encrypt.java

--- Java Program/Homework_033/Encrypt.java
+++ Java Program/Homework_033/Encrypt.java
@@ -5,36 +5,28 @@ public class Encrypt {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a line to be encrypted: ");
         String input = scanner.nextLine();
-        StringBuilder encrypted = new StringBuilder();
 
-        for (String word : input.split(" ")) {
-            if (word.length() > 2) {
-                // 交换首尾字符
-                char firstChar = word.charAt(0);
-                char lastChar = word.charAt(word.length() - 1);
-                String middle = word.substring(1, word.length() - 1);
+        if (input.length() < 3) {
+            System.out.println("The encryption is: " + input);
+        } else {
+            char firstChar = input.charAt(0);
+            char lastChar = input.charAt(input.length() - 1);
+            String middleSubstring = input.substring(1, input.length() - 1);
 
-                // 处理中间字符
-                StringBuilder shiftedMiddle = new StringBuilder();
-                for (char c : middle.toCharArray()) {
-                    shiftedMiddle.append((char) (c + 2));
+            StringBuilder encrypted = new StringBuilder();
+            encrypted.append(lastChar);
+
+            for (int i = 0; i < middleSubstring.length(); i++) {
+                char c = middleSubstring.charAt(i);
+                if (c != ' ') {
+                    encrypted.append((char) (c + 2));
+                } else {
+                    encrypted.append(' ');
                 }
-
-                encrypted.append(lastChar).append(shiftedMiddle).append(firstChar);
-            } else {
-                // 如果字符串长度小于3，直接添加
-                encrypted.append(word);
             }
-            encrypted.append(" "); // 单词之间添加空格
+
+            encrypted.append(firstChar);
+            System.out.println("The encryption is: " + encrypted.toString());
         }
-
-        // 移除最后一个多余的空格
-        if (encrypted.length() > 0 && encrypted.charAt(encrypted.length() - 1) == ' ') {
-            encrypted.deleteCharAt(encrypted.length() - 1);
-        }
-
-        System.out.println("The encryption is: " + encrypted.toString());
-
-        scanner.close();
     }
 }
