a5ab2de3e8dc4dded39febbe05ff988a10f0c3e8
Lu'cas <wuw81013@gmail.com>
2024-05-09 12:02:15 -0400

    Update ChangeHalf.java

--- Java Program/Homework_033/ChangeHalf.java
+++ Java Program/Homework_033/ChangeHalf.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
 
-public class ChangeHalf{
-    public static void main(String[] args){
-        Scanner myScanner = new Scanner(System.in);
-        String input = myScanner.nextLine();
+public class ChangeHalf {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
         String output;
 
-        if(input.length() % 2 == 0){
-            int middle = input.length()/2;
+        // Check if the length of the string is even or odd
+        if (input.length() % 2 == 0) {
+            // If even, split the string in half and swap
+            int middle = input.length() / 2;
             output = input.substring(middle) + input.substring(0, middle);
-            
+        } else {
+            // If odd, leave the middle character in place
+            int middle = input.length() / 2;
+            output = input.substring(middle + 1) + input.charAt(middle) + input.substring(0, middle);
         }
-        myScanner.close();
+
+        System.out.println("Output: " + output);
     }
 }
