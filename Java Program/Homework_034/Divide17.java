0ffd71966796d82bb610aff85ca359fe804bcf76
Lu'cas <wuw81013@gmail.com>
2024-05-09 09:28:07 -0400

    Update Divide17.java

--- Java Program/Homework_034/Divide17.java
+++ Java Program/Homework_034/Divide17.java
@@ -1,3 +1,25 @@
-public class Divide17 {
-    
+import java.util.Scanner;
+
+public class Divide17{
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number");
+
+        long firstNumber = myScanner.nextLong();
+
+        System.out.println("Enter the second number");
+
+        long secondNumber = myScanner.nextLong();
+
+        int length = String.valueOf(secondNumber).length();
+
+        double newNumber = firstNumber * Math.pow(10, length) + secondNumber;
+
+        double result = newNumber/17;
+
+        System.out.printf("The new number is: %.2f" , result);
+
+        myScanner.close();
+    }
 }
