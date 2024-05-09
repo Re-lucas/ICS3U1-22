73902a96f10708782324aeb7e7ae7694db0190b6
Lu'cas <wuw81013@gmail.com>
2024-05-09 14:20:02 -0400

    Update ChangeHalf.java

--- Java Program/Homework_033/ChangeHalf.java
+++ Java Program/Homework_033/ChangeHalf.java
@@ -7,6 +7,17 @@ public class ChangeHalf {
         String input = scanner.nextLine();
         String output;
 
+        //so we have two siuation:
+        //if teh input.length() is even than just simply divide into two even part by 
+        //.substring(0, middle) which is the part fron index zero to middle -1
+        // and another part of .substring(middle) which start from middle (include middle) and the rest after.
+
+        // second, if the input.length() is not even; 
+        // than the middle char does not change which is input.length() / 2 ;
+        // and the front of substring(0 , middle) and back substring(middle + 1) are changed place;
+
+
+
         // Check if the length of the string is even or odd
         if (input.length() % 2 == 0) {
             // If even, split the string in half and swap
