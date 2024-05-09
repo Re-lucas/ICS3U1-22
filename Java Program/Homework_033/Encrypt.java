722f52fdfe67a7296efb355e34922e9c6d72d269
Lu'cas <wuw81013@gmail.com>
2024-05-09 19:26:22 -0400

    Update Encrypt.java

--- Java Program/Homework_033/Encrypt.java
+++ Java Program/Homework_033/Encrypt.java
@@ -6,20 +6,25 @@ public class Encrypt {
         System.out.print("Enter a line to be encrypted: ");
         String input = myScanner.nextLine();
         
-        // 使用 split("") 方法将字符串分割为字符数组
+        //By using the String array to indicates that we need to split the string enter by user
+        // and also because we are comparing each of char, so using the String array to get index
         String[] characters = input.split("");
         
-        // 取得第一个字符
+        // get the first index [0] of array  element
         char firstChar = characters[0].charAt(0);
-        // 取得最后一个字符
+        // get the characters[characters.length - 1].charAt(0);
         char lastChar = characters[characters.length - 1].charAt(0);
         
-        // 输出最后一个字符
+        // print out the last char
         System.out.print(lastChar);
         
-        // 中间部分字符向后移动两位
+        // and the middle part of it move 2 positive in ACII
+        // the one not include index 0 and characters.length - 1;
         for (int i = 1; i < characters.length - 1; i++) {
+            //Gets the first character of the currently traversed string element.
             char c = characters[i].charAt(0);
+
+            //by checking if the char is empty or not 
             if (c != ' ') {
                 System.out.print((char) (c + 2));
             } else {
@@ -27,7 +32,7 @@ public class Encrypt {
             }
         }
         
-        // 输出第一个字符
+        // characters.length - 1;
         System.out.println(firstChar);
 
         myScanner.close();
