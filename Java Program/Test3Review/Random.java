1eb31635bb880e6611827a4bb86ca1d3d778dd80
Lu'cas <wuw81013@gmail.com>
2024-05-09 22:04:59 -0400

    Update Random.java

--- Java Program/Test3Review/Random.java
+++ Java Program/Test3Review/Random.java
@@ -4,11 +4,16 @@ public class Random{
     public static void main(String[] args){
         Scanner myScanner = new Scanner(System.in);
 
-
+        //Random Number Between Two Integers
         System.out.print("Enter two integers (a, b): ");
         int a = myScanner.nextInt();
         int b = myScanner.nextInt();
+
+        //by using a + Math.random() * ((b - a) + 1)) to indicate the range include the b and a output a random number
+        //example: b = 10 and a = 5; which is 5, if just time the thing and plus by a it will only include number 5 to a (10 - 1)
+        //so we need the ((b - a) + 1))
         int randomNumber = a + (int) (Math.random() * ((b - a) + 1));
+
         System.out.println("Random number between " + a + " and " + b + ": " + randomNumber);
 
         myScanner.close();
