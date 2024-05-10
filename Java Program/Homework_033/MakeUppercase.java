982c4626b80b1513ca378250eb5fb54ceec12853
Lu'cas <wuw81013@gmail.com>
2024-05-09 21:49:53 -0400

    Update MakeUppercase.java

--- Java Program/Homework_033/MakeUppercase.java
+++ Java Program/Homework_033/MakeUppercase.java
@@ -6,12 +6,23 @@ public class MakeUppercase {
         System.out.print("Enter string: ");
         String input = scanner.nextLine();
 
+        //loop for every char
         for (int i = 0; i < input.length(); i++) {
+            //initilize an char for comparing
             char character = input.charAt(i);
+
+            //set the range which needs to be convert
             if (character >= 'a' && character <= 'z') {
+
+                //because the ASCII code, so we first mins a and plus A
+                //Example: if a is number 78 (assuming) than the letter b is 79
+                //after mins a, left 1 and plus the A to get into to the parttern
                 character = (char)(character - 'a' + 'A');
+
+                //Ouput each one after converted
                 System.out.print(character);
             }else{
+                //if not in the range just output
                 System.out.print(input.charAt(i));
             }
         }
