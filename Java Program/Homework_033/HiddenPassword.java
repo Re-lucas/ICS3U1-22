d1bb160db2a56034c9dc3e07b888371c03ba4503
Lu'cas <wuw81013@gmail.com>
2024-05-09 20:06:13 -0400

    Update HiddenPassword.java

--- Java Program/Homework_033/HiddenPassword.java
+++ Java Program/Homework_033/HiddenPassword.java
@@ -2,14 +2,20 @@ import java.util.Scanner;
 
 public class HiddenPassword {
     public static void main(String[] args) {
+
+        //Get the user's string and the char wants to replace
         Scanner myScanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = myScanner.nextLine();
         System.out.print("Enter a character: ");
         char replacement = myScanner.next().charAt(0);
 
+        //using for loop to go over every char in the string,s so i < input.length();
         for (int i = 0; i < input.length(); i++) {
+            //char does not allow to compare with string, convert each index in the string into char
             char currentChar = input.charAt(i);
+
+            //check the situation if it is '' (space) or not
             if (currentChar == ' ') {
                 System.out.print(" "); // 输出空格
             } else {
