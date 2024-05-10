bfe61afa56bb23c557a70e025a2cfa40cabdb0ee
Lu'cas <wuw81013@gmail.com>
2024-05-09 22:31:04 -0400

    name chaged

--- Java Program/Test3Review/Vowels.java
+++ Java Program/Test3Review/Vowels.java
@@ -1,28 +1,24 @@
 import java.util.Scanner;
 
-public class Main {
+public class Vowels {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a line of text: ");
         String input = scanner.nextLine();
 
-        int vowelCount = countVowels(input);
-        System.out.println("Number of vowels: " + vowelCount);
-
-        scanner.close();
-    }
-
-    private static int countVowels(String input) {
         int count = 0;
-        // 将输入的字符串转换为小写字母，方便比较
+        // Convet the input strign into lower case by using toLowerCase() method 
         input = input.toLowerCase();
-        // 遍历字符串中的每个字符，检查是否为元音字母
+        // Iterate over each character in the string, checking for vowel letters
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
                 count++;
             }
         }
-        return count;
+
+        System.out.println("Number of vowels: " + count);
+
+        scanner.close();
     }
 }
