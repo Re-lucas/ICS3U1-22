f8cc26f4970e96e16e80fed1671b0afaa460bfd8
Lu'cas <wuw81013@gmail.com>
2024-05-09 20:19:48 -0400

    Update ReverseString.java

--- Java Program/Homework_033/ReverseString.java
+++ Java Program/Homework_033/ReverseString.java
@@ -6,6 +6,7 @@ public class ReverseString {
         System.out.print("Enter a string to be reversed: ");
         String input = myScanner.nextLine();
 
+        //is reversed which is loop from the last one and backward
         for (int i = input.length() - 1 ; i >= 0; i--){
             System.out.print(input.charAt(i));
         }
