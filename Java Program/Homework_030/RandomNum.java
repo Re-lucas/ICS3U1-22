c954b2b9da89e0809a54c4f8c7ce8cb9d9e6e276
Lu'cas <wuw81013@gmail.com>
2024-05-10 06:19:35 -0400

    Update RandomNum.java

--- Java Program/Homework_030/RandomNum.java
+++ Java Program/Homework_030/RandomNum.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class RandomNum {
     public static void main(String[] args) {
         Random random = new Random();
-        //or it can be int randomNumber = Math.random() * 100
+        //or it can be int randomNumber = (Math.random() * 100) + 1
         int randomNumber = random.nextInt(101); // Generates a number between 0 and 100
         System.out.println("Random number between 0 and 100: " + randomNumber);
     }
