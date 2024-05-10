7ac5f3cca14ff6266208ee5562ca618e2088fdcc
Lu'cas <wuw81013@gmail.com>
2024-05-10 11:01:32 -0400

    updata

---
+++ Java Program/Test3Review/WordChecker.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class WordChecker {
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter a long text: ");
+        String input = myScanner.nextLine();
+
+        // Split the input string into an array of words using space as the delimiter
+        String[] words = input.split("\\s+");
+
+        // Check each word and print if the first and last characters are the same
+        for(String word : words){
+            if (word.length() > 1 && word.charAt(0) == word.charAt(word.length() - 1)) {
+                System.out.println(word);
+            }
+        }
+
+        myScanner.close();
+    }
+}
