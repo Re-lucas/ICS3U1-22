4169c8f42faa44952ccac09d5d5aefd597706f2f
Lu'cas <wuw81013@gmail.com>
2024-05-13 10:52:38 -0400

    Update MethodExample.java

--- Java Program/Folder_Example/MethodExample.java
+++ Java Program/Folder_Example/MethodExample.java
@@ -1,11 +1,21 @@
 public class MethodExample {
     public static void main(String[] args){
+        printHello();
 
+        printHello("Mr Sir");
 
     }
-    
+
     public static void printHello(){
+        for(int i = 0; i<5 ; i++){
         System.out.println("Hello");
+        }
+    }
+
+    public static void printHello(String name){
+        System.out.println("Hello " + name);
     }
     
+    public static int sum (int num_1, int num_2){
+    }
 }
