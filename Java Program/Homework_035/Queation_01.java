39b8d80b061eacaa7b01dd651b7ff45183f5f396
Lu'cas <wuw81013@gmail.com>
2024-05-13 11:30:05 -0400

    Update Queation_01.java

--- Java Program/Homework_035/Queation_01.java
+++ Java Program/Homework_035/Queation_01.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class Queation_01 {
+
+
     public double translate(int value) {
         double num_1 = 0.0 + value;
         return num_1;
     }
 
-    
+    public int find(String text, double target) {
+        return find(text, target);
+    }
+
+    public void printAnswer(double a, double b, double c) {
+    }
     
 }
