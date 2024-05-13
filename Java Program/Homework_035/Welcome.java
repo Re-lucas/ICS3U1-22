229692adb4a06282e41cdfde0ab8a7e96c00016c
Lu'cas <wuw81013@gmail.com>
2024-05-13 11:34:03 -0400

    Create Welcome.java

---
+++ Java Program/Homework_035/Welcome.java
@@ -0,0 +1,11 @@
+public class Welcome {
+    public static String welcomeMessage(String name, int visitorNum) {
+        // Method body not implemented
+        // You can create the welcome message here
+        // For example:
+        // return "Welcome " + name + "! You are visitor number " + visitorNum + ".";
+
+        return welcomeMessage(name, visitorNum);
+    }    
+    
+}
