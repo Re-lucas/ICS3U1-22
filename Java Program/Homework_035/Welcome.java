f3df986dcb60558dfe663297370ab75c6eb1944e
Lu'cas <95332384+Re-lucas@users.noreply.github.com>
2024-05-20 17:17:50 -0400

    Update Welcome.java

--- Java Program/Homework_035/Welcome.java
+++ Java Program/Homework_035/Welcome.java
@@ -5,7 +5,7 @@ public class Welcome {
         // For example:
         // return "Welcome " + name + "! You are visitor number " + visitorNum + ".";
 
-        return 0;
+        return name;
     }    
     
 }
