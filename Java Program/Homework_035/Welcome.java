77e6ebb0950c7ed33ddb0e9fd0d174c3277626a9
Lu'cas <95332384+Re-lucas@users.noreply.github.com>
2024-05-26 17:38:00 -0400

    Update Welcome.java

--- Java Program/Homework_035/Welcome.java
+++ Java Program/Homework_035/Welcome.java
@@ -1,11 +1,19 @@
 public class Welcome {
+
+    public static void main(String[] args) {
+        String name = "Lucas";
+        int visitorNum = 1;
+        System.out.println(welcomeMessage(name, visitorNum));
+    }
+
+    //the question ask for a method to get the variable of a name(String) and number of visitor (int)
+    //and return back to a String of serval txt, which mean the method need to be in String type. 
+ 
     public static String welcomeMessage(String name, int visitorNum) {
-        // Method body not implemented
-        // You can create the welcome message here
-        // For example:
         // return "Welcome " + name + "! You are visitor number " + visitorNum + ".";
 
-        return name;
+        String Hello = "Welcome " + name + "! You are visitor number " + visitorNum + ".";
+
+        return Hello;
     }    
-    
 }
