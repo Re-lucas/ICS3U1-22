cb43c3064d42b252e49d8693ef4eba697c29bad7
Lu'cas <95332384+Re-lucas@users.noreply.github.com>
2024-05-26 19:33:34 -0400

    Update SoundAlarm.java

--- Java Program/Homework_035/SoundAlarm.java
+++ Java Program/Homework_035/SoundAlarm.java
@@ -5,6 +5,9 @@ public class SoundAlarm {
     }
 
     public static void alarm(int noOfTimes) {
+
+        //So the question is to determine if the number of Times is equal to 1 or not
+        //if equal or grater than just for i loop., if not return error
         if (noOfTimes < 1) {
             System.out.println("Error: Parameter must be greater than or equal to 1.");
         } else {
