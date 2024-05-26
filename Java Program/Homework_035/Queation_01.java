887845814f45ec78c5f2395a9c8321ca4fe3250f
Lu'cas <95332384+Re-lucas@users.noreply.github.com>
2024-05-26 17:31:58 -0400

    Update Queation_01.java

--- Java Program/Homework_035/Queation_01.java
+++ Java Program/Homework_035/Queation_01.java
@@ -1,15 +1,22 @@
 public class Queation_01 {
 
-
+    //the question ask for a int number which is value in of the variable in this method,
+    //by double num_1 = 0.0 + value to convert it into double,
+    //in the return which return back the num_1 with it's double type.
     public double translate(int value) {
         double num_1 = 0.0 + value;
         return num_1;
     }
 
+
+    //the question ask the letting the method return back int which means to make the public static int
+    //and also ask for two avriable, one String type one double
     public int find(String text, double target) {
-        return find(text, target);
+        int num_1 = 0;
+        return num_1;
     }
 
+    //Write a method header for a method named printAnswerthat takes three doubles as parameters and does not return anything.
     public void printAnswer(double a, double b, double c) {
     }
     
