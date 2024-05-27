f3840c3dd0e45ad623e84cffa7ddade0f047ec87
Lu'cas <95332384+Re-lucas@users.noreply.github.com>
2024-05-26 21:49:34 -0400

    Update ReverseString.java

--- Java Program/Homework_033/ReverseString.java
+++ Java Program/Homework_033/ReverseString.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 
 public class ReverseString {
     public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
-        System.out.print("Enter a string to be reversed: ");
-        String input = myScanner.nextLine();
-
-        //is reversed which is loop from the last one and backward
-        for (int i = input.length() - 1 ; i >= 0; i--){
-            System.out.print(input.charAt(i));
+        try (Scanner myScanner = new Scanner(System.in)) {
+            System.out.print("Enter a string to be reversed: ");
+            
+            if (myScanner.hasNextLine()) {
+                String input = myScanner.nextLine();
+                
+                //is reversed which is loop from the last one and backward
+                for (int i = input.length() - 1; i >= 0; i--) {
+                    System.out.print(input.charAt(i));
+                }
+            } else {
+                System.out.println("No input provided.");
+            }
         }
-
-        myScanner.close();
-
     }
 }
