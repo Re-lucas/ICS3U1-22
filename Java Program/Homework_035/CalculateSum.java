318dee660ca1485d3fb8985d4866d389cd13a714
Lu'cas <95332384+Re-lucas@users.noreply.github.com>
2024-05-26 20:56:42 -0400

    Update CalculateSum.java

--- Java Program/Homework_035/CalculateSum.java
+++ Java Program/Homework_035/CalculateSum.java
@@ -6,10 +6,14 @@ public class CalculateSum {
     }
 
     public static int sumRange(int start, int end) {
+
+        //here ask for a int type method and determine the sencond number have to be larger than teh first number
         if (end < start) {
             System.out.println("Error: Second parameter must be greater than or equal to the first.");
             return 0;
         } else {
+            //if the second num larger than the first num
+            // it will get the sum betweeen and include of this two number
             int sum = 0;
             for (int i = start; i <= end; i++) {
                 sum += i;
