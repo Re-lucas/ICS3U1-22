95ba8b263755f78c6b91216d0d2f17fba677932d
Lu'cas <95332384+Re-lucas@users.noreply.github.com>
2024-05-26 21:06:03 -0400

    Update ReverseString.java

--- Java Program/Homework_036/ReverseString.java
+++ Java Program/Homework_036/ReverseString.java
@@ -2,18 +2,16 @@ import java.util.Scanner;
 
 public class ReverseString {
     public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
-        String input = "";
-
-        System.out.println("Enter a string:");
-        input = myScanner.nextLine();
-
-        System.out.println(reverseString(input));
-
-        myScanner.close();
+        try (Scanner myScanner = new Scanner(System.in)) {
+            System.out.println("Enter a string:");
+            String input = myScanner.nextLine();
+            
+            System.out.println(reverseString(input));
+        }
     }
 
     public static String reverseString(String string) {
+        //because the question ask for reverse the string which just print each backward and combine it
         String newString = ""; // Initialize an empty string to store the reversed result
 
         for (int i = string.length() - 1; i >= 0; i--) {
