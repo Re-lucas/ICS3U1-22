43c6f483ba639882b8dad3d6c013495b6d65a914
Lu'cas <95332384+Re-lucas@users.noreply.github.com>
2024-05-26 21:07:38 -0400

    Update SameReverse.java

--- Java Program/Homework_036/SameReverse.java
+++ Java Program/Homework_036/SameReverse.java
@@ -1,15 +1,25 @@
 public class SameReverse {
-    public static boolean sameReverse(String s) {
-        if (s == null) {
-            return true;
-        }
 
-        String reversed = new StringBuilder(s).reverse().toString();
-        return s.equalsIgnoreCase(reversed);
+    // Method to reverse a given string
+    public static String reverse(String s) {
+        // Using StringBuilder to reverse the string
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    // Method to check if the string and its reverse are the same
+    public static boolean sameReverse(String s) {
+        // Getting the reversed string
+        String reversed = reverse(s);
+        // Comparing the original string with the reversed string
+        return s.equals(reversed);
     }
 
     public static void main(String[] args) {
-        String testString = "piano";
-        System.out.println(sameReverse(testString)); // Output: true
+        // Test cases
+        String test1 = "madam";
+        String test2 = "hello";
+        
+        System.out.println("Is \"" + test1 + "\" the same as its reverse? " + sameReverse(test1)); // true
+        System.out.println("Is \"" + test2 + "\" the same as its reverse? " + sameReverse(test2)); // false
     }
 }
