13b284421c71880024c6ad341cac4e87b71709f5
Lu'cas <95332384+Re-lucas@users.noreply.github.com>
2024-06-06 10:05:15 -0400

    Update Battleship.java

--- Final Project/Battleship.java
+++ Final Project/Battleship.java
@@ -231,7 +231,7 @@ public class Battleship {
         }
 
 
-        //Problem unslove: as the variable got from method placeShips
+        //Problem unslove(sloved): as the variable got from method placeShips
         //the variable in the commander as for x is assgined to y and the y is assigned to x 
         public boolean placeShip(int x, int y, int size, char direction) {
             if (direction == 'h') {
